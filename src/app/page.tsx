f6480f9bfec2b3d8c1b10561960539f00935a8e5
Rowan Murray <rowanomurray@gmail.com>
2025-03-08 17:18:57 +0000

    feat: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-import { auth, signIn } from '@/auth'
-
 export default async function Home() {
-    const session = await auth()
-    console.log(session)
-    return (
-        <div className="min-h-screen bg-gray-100 grid place-items-center">
-            <form
-                action={async () => {
-                    'use server'
-                    await signIn('google')
-                }}
-            >
-                <button type="submit" className="bg-red-400">
-                    Signin with Google
-                </button>
-            </form>
-        </div>
-    )
+    return <div className="h-full grid place-items-center">content</div>
 }
