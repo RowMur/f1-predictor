c1cfeeb6e759ffae2fbf729d6c81d34afb375681
Rowan Murray <rowanomurray@gmail.com>
2025-03-09 12:13:08 +0000

    feat: driver images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import { getRaces } from '@/modules/f1-api/getters'
+import { getDrivers, getRaces } from '@/modules/f1-api/getters'
+import Image from 'next/image'
 
 export default async function Home() {
     const currentYear = new Date().getFullYear()
     const races = await getRaces(currentYear)
     const upcomingRace = races.find((r) => r.date > new Date().toISOString())
+    const drivers = await getDrivers(currentYear)
+    console.log(drivers)
     return (
-        <div className="flex justify-center">
+        <div className="flex items-center flex-col">
             <div className="bg-light-gray max-w-96 mx-6 my-16 p-6 rounded-lg">
                 {upcomingRace ? (
                     <>
@@ -24,6 +27,22 @@ export default async function Home() {
                 ) : (
                     <h2 className="font-bold">No upcoming races</h2>
                 )}
+            </div>
+            <div>
+                <h2 className="font-bold">Drivers</h2>
+                <ul className="flex flex-wrap max-w-96">
+                    {drivers.map((driver) => (
+                        <li key={driver.driverId}>
+                            {driver.givenName} {driver.familyName}
+                            <Image
+                                src={`/drivers/${driver.driverId}.avif`}
+                                alt={`${driver.givenName}'s picture`}
+                                width={100}
+                                height={100}
+                            />
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
--- src/modules/f1-api/getters.ts
+++ src/modules/f1-api/getters.ts
@@ -1,4 +1,4 @@
-import { RaceResponseData } from './types'
+import { DriversResponseData, RaceResponseData } from './types'
 import { endpoint } from './constants'
 
 export const getRaces = async (year: number) => {
@@ -7,4 +7,12 @@ export const getRaces = async (year: number) => {
     })
     const body: RaceResponseData = await res.json()
     return body.MRData.RaceTable.Races
+}
+
+export const getDrivers = async (year: number) => {
+    const res = await fetch(`${endpoint}/${year}/drivers/`, {
+        cache: 'force-cache',
+    })
+    const body: DriversResponseData = await res.json()
+    return body.MRData.DriverTable.Drivers
 }
